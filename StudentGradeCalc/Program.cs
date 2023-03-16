d9ea677b14dc2dde456ee09c659ca0b8196d3b6d
BrandonCalitz-VC <ST10039352@vcconnect.edu.za>
2023-03-16 13:07:27 +0200

    Student Grade Calculations (Not finished)

---
+++ StudentGradeCalc/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace StudentGradeCalc
+{
+  internal static class Program
+  {
+    [STAThread]
+    static void Main()
+    {
+      while (true)
+      {
+        string inP = Interaction.InputBox("Input the amount of students", "Students", "");
+        int? NumStudents = null;
+        if (int.TryParse(inP, out var pNum))
+        { 
+          
+          break; 
+        }
+        MessageBox.Show("Invalid Input");
+      }
+      for (int i = 0; i < numSt; i++) { }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+    }
+  }
+}
