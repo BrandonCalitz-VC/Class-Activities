6134520f9c8ce4cdb85e3889d3689487ac582bb7
BrandonCalitz-VC <ST10039352@vcconnect.edu.za>
2023-03-23 13:05:03 +0200

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿//base class for vehicles
+public class Vehicle
+{
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public int Year { get; set; }
+  public double MaxSpeed { get; set; }
+  public string Type { get; set; }
+  public int SeatCapacity { get; set; }
+
+  public Vehicle(string Make, string Model, int Year, double MaxSpeed, string Type, int SeatCapacity) 
+  {
+    this.Make = Make;
+    this.Model = Model;
+    this.Year = Year;
+    this.MaxSpeed = MaxSpeed;
+    this.Type = Type;
+    this.SeatCapacity = SeatCapacity;
+  }
+
+  //Virtual allows method to be overridden]
+
+  public void startEngine()
+  {
+        Console.WriteLine("Starting Engine...");
+  }
+
+  public void stopEngine() 
+  { 
+    Console.WriteLine("Stopping Engine...");
+  }
+}
